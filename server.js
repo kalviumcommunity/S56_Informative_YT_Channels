b280959c349e9b6c9d4f8e5aceb7de41dc333b4b
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2024-02-27 09:26:46 +0530

    CRUDDone

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const Router = require('./routes'); 
+const port = process.env.PORT || 3000;
 const { connected, isConnected } = require('./config/dB');
 
+app.use('/', Router); 
 app.get('/', (req, res) => {
-  try{
-      res.json({
-          database : isConnected() ? 'connected' : 'disconnected'}
-      )
+  try {
+    res.json({
+      database: isConnected() ? 'connected' : 'disconnected',
+    });
+  } catch (err) {
+    console.log(err);
   }
-  catch(err){
-      console.log(err)
-  }
-})
-app.get('/ping',(req,res)=>{
-  try{
-      res.send("Hello")
-  }
-  catch(err){
-      console.log(err)
-  }
-})
-if (require.main === module) {
-  connected()
-  app.listen(port, async () => {
-    console.log("⁠🚀 server running on PORT: ${port}");
 });
+
+app.get('/ping', (req, res) => {
+  try {
+    res.send('Hello');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+if (require.main === module) {
+  connected();
+  app.listen(port, async () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
 }
