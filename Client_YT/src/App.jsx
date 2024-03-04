c49c7d6a3c9d08e4b8972084b80724c64e2924e6
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2024-03-04 11:54:28 +0530

    Connected Server To FrontEnd

--- Client_YT/src/App.jsx
+++ Client_YT/src/App.jsx
@@ -1,36 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 import DummyData from './Components/DummyData';
-import {Router, Routes, Route} from 'react-router-dom'
 import Data from './data.json'
+import Main from './Pages/main';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <nav className="navbar">
-        <div className="logo">
-          <img src="./Images/YTlogo.jpg" alt="" />
-        </div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-
-      <div className="main-container">
-        <h1 className="main-heading">List of Most Informative YouTube Channels</h1>
-        <p className="current-page">Current Page: Home</p>
-
-        <div className="search-bar">
-          <input type="text" placeholder="Search..." />
-        </div>
-      </div>
-      <div className='dummydata'>
+      <Main/>
+      {/* Uncomment Only if you want to see Dummy Data */}
+      {/* <div className='dummydata'>
       <DummyData data={Data} />
-      </div>
+      </div> */}
       
     </>
   );
